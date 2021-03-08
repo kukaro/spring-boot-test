8d86810f136a055094961e8f27a75f1385e113da
Coeat.k <coeat.k@kakaocorp.com>
2021-03-08 17:02:48 +0900

    feat: add mybatis-test

---
+++ mybatis-test/src/test/java/com/example/coeat/CoeatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coeat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoeatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
